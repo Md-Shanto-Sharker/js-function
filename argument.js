6175e8cd46f034bd87c12f8f5e66d72132dda7f2
SHANTO <rabbymd55555@gmail.com>
2025-02-13 14:40:08 +0600

    added return.js file

--- argument.js
+++ argument.js
@@ -1,31 +1,24 @@
-function doubleIt ( number){
-    const double = number*2;
-    console.log(number,double);
+function doubleIt(number) {
+  const double = number * 2;
+  console.log(number, double);
 }
-console.log('I will call the function');
+console.log("I will call the function");
 doubleIt(15);
-console.log('---------------------------');
+console.log("---------------------------");
 doubleIt(88);
-console.log('-----------------------------');
+console.log("-----------------------------");
 doubleIt(873);
-console.log('-----------------------------');
+console.log("-----------------------------");
 const number = 55;
 doubleIt(number);
-console.log('-----------------------------');
+console.log("-----------------------------");
 const money = 112;
 doubleIt(money);
 
-
-
-
-
-
-
-
-function difference (num1,num2){
-    const diff  =num1 - num2;
-    console.log(num1,num2, 'difference is',diff);
+function difference(num1, num2) {
+  const diff = num1 - num2;
+  console.log(num1, num2, "difference is", diff);
 }
-const fatherAge= 50;
+const fatherAge = 50;
 const myAge = 10;
-difference(fatherAge,myAge)
+difference(fatherAge, myAge);
---
+++ return.js
@@ -0,0 +1,15 @@
+function tenTimes(number){
+    const result = number * 10;
+    return result;
+}
+
+function cutHalf(number){
+    const half = number/2;
+    return half;
+}
+
+const output = tenTimes(10);
+console.log(output);
+
+const output2 = cutHalf(20);
+console.log(output2);

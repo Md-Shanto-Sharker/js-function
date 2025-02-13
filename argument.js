93d9c9900baef60badd2e346320e184840a8aaf9
SHANTO <rabbymd55555@gmail.com>
2025-02-13 11:17:55 +0600

    added argument.js file

---
+++ argument.js
@@ -0,0 +1,31 @@
+function doubleIt ( number){
+    const double = number*2;
+    console.log(number,double);
+}
+console.log('I will call the function');
+doubleIt(15);
+console.log('---------------------------');
+doubleIt(88);
+console.log('-----------------------------');
+doubleIt(873);
+console.log('-----------------------------');
+const number = 55;
+doubleIt(number);
+console.log('-----------------------------');
+const money = 112;
+doubleIt(money);
+
+
+
+
+
+
+
+
+function difference (num1,num2){
+    const diff  =num1 - num2;
+    console.log(num1,num2, 'difference is',diff);
+}
+const fatherAge= 50;
+const myAge = 10;
+difference(fatherAge,myAge)
---
+++ parameter.js
@@ -0,0 +1,12 @@
+// function square(number){
+//     const borgo = number * number;
+//     console.log(borgo);
+    
+// }
+// square(4)
+
+function sum(a,b){
+    let result = a + b;
+    console.log(result);
+}
+sum(10,10)
